8966172e3bf5f7ca809f33c4a9fc1abc711fb972
adiop2 <abdallah.diop@neticoa.sn>
2016-06-09 09:50:41 +0000

    refs #16622 :correction bug

--- assets/javascripts/test_case_tree.js
+++ assets/javascripts/test_case_tree.js
@@ -194,7 +194,14 @@ jQuery(document).ready(function ($) {
 				dialog[node_type].unbind("dialogbeforeclose");
 				node.attr("id", "node_" + id);
 				node.data("jstree", (node_type=='test_case')?LEAF_MENU:FOLDER_MENU);
+					if ($.jstree._reference(node) != null){
 					$.jstree._reference(node).set_text(node, tc["node[name]"]);
+
+					}else
+					{
+						$.jstree._reference(0).set_text(node, tc["node[name]"]);
+					}
+
 			    });
 			    dialog[node_type].dialog('close');
 			    show_notification_dialog(r.status, r.message);
