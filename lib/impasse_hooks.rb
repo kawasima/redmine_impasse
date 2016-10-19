23cc969b62811d124110fdf2c47358c28570a819
Fabrice Robin <fabrice.robin@neticoa.fr>
2016-10-19 12:39:03 +0200

    Allow to create new issue without project

--- lib/impasse_hooks.rb
+++ lib/impasse_hooks.rb
@@ -64,6 +64,7 @@ module ImpassePlugin
         return '' unless issue.project && !issue.project.blank? && issue.project.module_enabled?('impasse')
 
         project = context[:project]
+        return '' unless project && project.blank? && project.module_enabled?('impasse')
         
         setting = Impasse::Setting.find_by(:project_id => project.id) || Impasse::Setting.create(:project_id => project.id)
 
